ef0e1472f13da714a0aec818218bc808f7cd919f
dude19831 <carsten@eventjive.co>
2015-10-19 15:15:10 +0000

    Changes in text and added mockups

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//Database Information
+define(DB_HOST, "127.0.0.1");
+define(DB_USER, "dude19831");
+define(PASSWORD, "Hkdscpl1983");
+define(DATABASE, "c9");
+//Site base
+define(URL, "http://www.eventjive.co"); //DO NOT FORGET THE TRAILING /
+?>
